d9f954d07c0e96582d46681ca9366cb4ae47d9ea
tiko <tikotas@mail.ru>
2023-02-19 14:17:17 +0400

    fixed several issues

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import "./Header.css"
-function Header() {
+import {store} from "../../store/store";
+
+const Header = () => {
 
     /* Toggle Menu */
 
@@ -12,61 +14,27 @@ function Header() {
             <nav className="nav container">
                 <a href="index.html" className="nav__logo">David</a>
 
-                <div className={
-                    showMenu
-                        ? "nav__menu show-menu"
-                        : "nav__menu"
-                }>
+                <div className={showMenu ? "nav__menu show-menu" : "nav__menu"}>
                     <ul className="nav__list grid">
-
-                        <li className="nav__item">
-                            <a href="#home" className="nav__link active__link">
-                                <i className="uil uil-estate nav__icon"></i>
-                                Home
-                            </a>
-                        </li>
-
-                        <li className="nav__item">
-                            <a href="#about" className="nav__link">
-                                <i className="uil uil-user nav__icon"></i>
-                                About
-                            </a>
-                        </li>
-
-                        <li className="nav__item">
-                            <a href="#skills" className="nav__link">
-                                <i className="uil uil-file-alt nav__icon"></i>
-                                Skills
-                            </a>
-                        </li>
-
-                        <li className="nav__item">
-                            <a href="#services" className="nav__link">
-                                <i className="uil uil-briefcase-alt nav__icon"></i>
-                                Services
-                            </a>
-                        </li>
-
-                        <li className="nav__item">
-                            <a href="#portfolio" className="nav__link">
-                                <i className="uil uil-scenery nav__icon"></i>
-                                Portfolio
-                            </a>
-                        </li>
-
-                        <li className="nav__item">
-                            <a href="#contact" className="nav__link">
-                                <i className="uil uil-message nav__icon"></i>
-                                Contact
-                            </a>
-                        </li>
+                        {
+                            store.menus.map(menu => {
+                                return (
+                                    <li key={menu.id} className="nav__item">
+                                        <a href={menu.page} className="nav__link">
+                                            {menu.Icon}
+                                            {menu.title}
+                                        </a>
+                                    </li>
+                                )
+                            })
+                        }
                     </ul>
 
-                    <i className="uil uil-times nav__close" onClick={() => setShowMenu(!showMenu)}></i>
+                    <i className="uil uil-times nav__close" onClick={() => setShowMenu(!showMenu)}/>
                 </div>
 
                 <div className="nav__toggle" onClick={() => setShowMenu(!showMenu)}>
-                    <i className="uil uil-apps"></i>
+                    <i className="uil uil-apps"/>
                 </div>
             </nav>
 
