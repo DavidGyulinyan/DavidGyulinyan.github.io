d757dee69ca110743d386f12330e62feac8df33a
DavidGyulinyan <davidgyulinyan@gmail.com>
2023-03-07 21:10:11 +0400

    Disable scrolling while opened Portfolio items and Services items

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,21 +1,14 @@
-import React, { useState, useEffect } from "react";
-import "./Modal.css";
+import React, {useState} from "react"
+import "./Modal.css"
 import ModalServices from "../ModalServices/ModalServices";
 
-const Modal = ({ product }) => {
-    const [toggleState, setToggleState] = useState(0);
+const Modal = ({product}) => {
+
+    const [toggleState, setToggleState] = useState(0)
 
     const toggleTab = (index) => {
-        setToggleState(index);
-    };
-
-    useEffect(() => {
-        if (toggleState === product.id) {
-            document.body.style.overflow = "hidden";
-        } else {
-            document.body.style.overflow = "";
-        }
-    }, [toggleState, product.id]);
+        setToggleState(index)
+    }
 
     return (
         <div className="services__content">
@@ -25,23 +18,22 @@ const Modal = ({ product }) => {
                 </i>
             </div>
             <span className="services__button" onClick={() => toggleTab(product.id)}>
-        View More
-        <i className="uil uil-arrow-right services__button-icon" />
-      </span>
-            <div
-                className={
-                    toggleState === product.id ? "services__modal active-modal" : "services__modal"
-                }
-            >
+                View More
+                <i className="uil uil-arrow-right services__button-icon"/>
+            </span>
+            <div className={toggleState === product.id ? "services__modal active-modal" : "services__modal"}>
                 <div className="services__modal-content">
-                    <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close" />
+                    <i
+                        onClick={() => toggleTab(0)}
+                        className="uil uil-times services__modal-close"
+                    />
                     <h3 className="services__modal-title">{product.title2}</h3>
                     <p className="services__modal-description">{product.description}</p>
-                    <ModalServices services={product.modalServiceItems} />
+                    <ModalServices services={product.modalServiceItems}/>
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Modal;
+export default Modal
