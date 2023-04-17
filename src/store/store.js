c8654f4b4cd6900cd782deb4ae731db7e11be376
DavidGyulinyan <davidgyulinyan@gmail.com>
2023-04-17 22:03:15 +0400

    added new CV and new figma project

--- src/store/store.js
+++ src/store/store.js
@@ -9,6 +9,7 @@ import Work2 from "../assets/work2.png";
 import Work3 from "../assets/work3.png";
 import Work4 from "../assets/work4.png";
 import Work5 from "../assets/work5.png";
+import Work6 from "../assets/work6.png";
 
 
 export const store = {
